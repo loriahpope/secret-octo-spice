71ddac56a2b6a130da1432fc301f58623b0f0917
Loriah Pope <llp260@nyu.edu>
2014-10-06 19:05:21 -0400

    Update expressHW.js

--- expressHW.js
+++ expressHW.js
@@ -1,25 +1,21 @@
 var express = require('express');
 var app = express();
-var path = require("path");
-var publicPath = path.resolve(__dirname, "public");
-app.use(express.static(publicPath));
-
-//Handlebars
-var handlebars = require('express3-handlebars').create({ defaultLayout:'main' });
-app.engine('handlebars', handlebars.engine);
+var handlebars = require('express3-handlebars');
+app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+app.use('/public', express.static('public'));
 
-
-app.get('/', function(req, res){
-res.render('index', greeting);
-	res.send('hello');
+app.get('/home', function(req, res){
+	res.render('index',{
+		name: 'World'
+	});
 });
 
-app.get('/about', function(req, res) {
-	res.send('you has q, i has answer');
+app.get('/about', function(req, res){
+	res.render('about');
 });
 
-//listen on port 3000
-app.listen(3000);
-console.log('Started server on port 3000');
+var port = 3000;
+app.listen(port);
+console.log('Starting server on port', port);
